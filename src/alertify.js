7559287475daa34d29aac832bdf79e4cc7d84c88
Onumah Kalu Samuel <onumahkalusamuel@gmail.com>
2021-05-27 09:00:15 +0100

    Fixed size tracking in alertify.js

--- src/alertify.js
+++ src/alertify.js
@@ -1,10 +1,11 @@
-import { title, message, type, showing } from './store'
+import { title, message, type, size, showing } from './store'
 
 const alertify = (args) => {
 
     if(typeof args === 'object') {
         title.set(args.title || 'Alert');
         message.set(args.message || '');
+        size.set(args.size || 'small');
         type.set(args.type || 'neutral');
 
     } else {
